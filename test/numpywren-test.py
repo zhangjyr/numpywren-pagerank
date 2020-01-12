782c2304aca49334ab8717f306b45d5d8133334d
AdamLatos <latosadam1@gmail.com>
2020-01-12 23:11:44 +0000

    add demo

--- test/numpywren-test.py
+++ test/numpywren-test.py
@@ -1,55 +1,38 @@
-# Get numpy results for matrix multiplication:
 import numpy as np
-import time
 
-N = 10000
-shard_size = (5000,5000)
-print(f"N = {N}")
-print(f"shard_size = {shard_size}")
+# params
+N = 4
+shard_size = (4,4)
+# print(f"N = {N}")
+# print(f"shard_size = {shard_size}")
 
 np.random.seed(42)
 X = np.random.randn(N,N)
 
-start = time.time()
-XXT_np = X @ X.T
-end = time.time()
-print("time:")
-print(end - start)
 
-#print("Numpy result: ")
-#print(XXT_np)
+# numpy:
+print("Running numpy version...")
+XX_np = X @ X
 
 
-# Test if pywren works:
-
+# pywren:
+print("Running pywren version...")
 
 import pywren
 
+def f(x): 
+    return X @ X
+
 pwex = pywren.lambda_executor()
 
-'''
-def f(x): 
-    return X @ X.T
-
-
 futures = pwex.map(f, range(1))
-
-print("Testing pywren:")
-start = time.time()
 pywren.wait(futures)
-end = time.time()
-print("time:")
-print(end - start)
-
 results = [f.result(throw_except=False) for f in futures]
-XXT_pywren = results[0]
-'''
-
-#print("Pywren result:")
-#print(XXT_pywren)
+XX_pywren = results[0]
 
 
-# Numpywren
+# numpywren:
+print("Running numpywren version...")
 
 from numpywren.matrix import BigMatrix
 from numpywren.matrix_init import shard_matrix
@@ -57,12 +40,14 @@ from numpywren.binops import gemm
 
 X_sharded = BigMatrix("mulitply_test", shape=X.shape, shard_sizes=shard_size)
 shard_matrix(X_sharded, X)
+XX_sharded = gemm(pwex, X_sharded, X_sharded, X_sharded.bucket, 1)
+XX_numpywren = XX_sharded.numpy()
 
-print("Testing numpywren:")
-start = time.time()
-XXT_sharded = gemm(pwex, X_sharded, X_sharded.T, X_sharded.bucket, 1)
-end = time.time()
-print("time:")
-print(end - start)
-#print("Numpywren result ")
-#print(XXT_sharded.numpy())
+print("Numpy result: ")
+print(XX_np)
+
+print("Pywren result:")
+print(XX_pywren)
+
+print("Numpywren result ")
+print(XX_numpywren)
